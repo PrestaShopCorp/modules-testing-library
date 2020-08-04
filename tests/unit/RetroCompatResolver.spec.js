a409208b947c96586b9a3fe556ffb8d6ca67809e
David Piatek <david.piatek@prestashop.com>
2020-08-04 12:23:58 +0200

    test resolver retro-compat

--- tests/unit/RetroCompatResolver.spec.js
+++ tests/unit/RetroCompatResolver.spec.js
@@ -1,31 +1,22 @@
-/* eslint-disable
+/* eslint-disable */
 
-const RetroCompatResolver = require('resolvers/RetroCompatResolver.js');
-
-//override
-//const overrideMapping = require('./class-extends');
-const overrideMapping = require('./tests/units/class-extends.js');
-
-//petet inutile
-getBaseDir(){
-    return '/home/david/Workspace/PrestaShop/poc-extends-qa';
-}
-
-let retroCompatResolver = RetroCompatResolver('177');
-expect(retroCompatResolver.getRequire('youpi.js')).to.equal('youpi.js');
-expect(retroCompatResolver.getRequire('tralala.js')).to.equal('tralala-override.js');
-
-let retroCompatResolver = RetroCompatResolver('178');
-expect(retroCompatResolver.getRequire('youpi.js')).to.equal('youpi.js');
-expect(retroCompatResolver.getRequire('tralala.js')).to.equal('tralala.js');
-*/
+const RetroCompatResolver = require('../../resolvers/RetroCompatResolver.js');
 const assert = require('assert');
+const overrideMapping = require('./class-extends');
 
-describe('Basic Mocha String Test', function () {
- it('should return number of charachters in a string', function () {
-        assert.equal("Hello".length, 5);
+describe('Testing resolver retroCompact', function () {
+    it('should call method with class extends', function () {
+        const retroCompatResolver = new RetroCompatResolver('177');
+        // method not override
+        assert.equal(retroCompatResolver.getRequire('youpi.js', overrideMapping), process.cwd() + '/' + 'youpi.js');
+        // method override in this prestashop version
+        assert.equal(retroCompatResolver.getRequire('tralala.js', overrideMapping), 'tralala-override.js');
     });
- it('should return first charachter of the string', function () {
-        assert.equal("Hello".charAt(0), 'H');
+    it('should not call method with class extends', function () {
+        const retroCompatResolver = new RetroCompatResolver('178');
+        // method not override
+        assert.equal(retroCompatResolver.getRequire('youpi.js', overrideMapping), process.cwd() + '/' + 'youpi.js');
+        // method not override in this prestashop version
+        assert.equal(retroCompatResolver.getRequire('tralala.js', overrideMapping), process.cwd() + '/' + 'tralala.js');
     });
 });
--- tests/unit/class-extends.js
+++ tests/unit/class-extends.js
@@ -2,6 +2,6 @@ module.exports = {
   'tralala.js__177': {
     version: 177,
     objName: 'tralala',
-    filepath: '/tralala-override.js',
+    filepath: 'tralala-override.js',
   },
 };
