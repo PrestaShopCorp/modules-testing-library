e11147a93a137d85acb607d91fafb69d8234086e
Boubker BRIBRI <boubker.bribri@prestashop.com>
2020-08-17 14:28:07 +0200

    Add boBasePage to class map

--- kernel/resolvers/configClassMap.js
+++ kernel/resolvers/configClassMap.js
@@ -1,13 +1,18 @@
 module.exports = [
   {
     version: 'common',
-    selector: 'kernel/common/BO/login/index.js',
-    filepath: '@kernel/common/BO/login/index.js',
+    selector: 'kernel/common/BO/BObasePage.js',
+    filepath: '@kernel/common/BO/BObasePage.js',
   },
   {
-    version: '1.7.8',
+    version: '1.7.8.0',
+    selector: 'kernel/common/BO/BObasePage.js',
+    filepath: '@versions/v178/BO/BObasePage.js',
+  },
+  {
+    version: 'common',
     selector: 'kernel/common/BO/login/index.js',
-    filepath: '@versions/v178/BO/login/index.js',
+    filepath: '@kernel/common/BO/login/index.js',
   },
   {
     version: 'common',
