aa16812ee30df3dbc771823d591e78f1c22c6b0c
Simon Garny <simon.garny@prestashop.com>
2020-09-23 18:13:36 +0200

    refacto configclassmap

--- kernel/resolvers/configClassMap.js
+++ kernel/resolvers/configClassMap.js
@@ -1,97 +1,108 @@
 module.exports = [
   {
-    version: 'common',
-    selector: 'BO/BObasePage.js',
-    filepath: '@versions/common/BO/BObasePage.js',
+    file: 'BO/BObasePage.js',
+    combinations: {
+      common: {
+        type: 'filepath',
+        target: '@versions/common/BO/BObasePage.js',
+      },
+      '1.7.8.0': {
+        type: 'filepath',
+        target: '@versions/v178/BO/BObasePage.js',
+      },
+      '1.7.3.4': {
+        type: 'filepath',
+        target: '@versions/v173/BO/BObasePage.js',
+      },
+      '1.7.2.5': {
+        type: 'filepath',
+        target: '@versions/v172/BO/BObasePage.js',
+      },
+      '1.7.1.2': {
+        type: 'version',
+        target: '1.7.2.5',
+      },
+      '1.7.0.6': {
+        type: 'version',
+        target: '1.7.2.5',
+      },
+    },
   },
   {
-    version: '1.7.8.0',
-    selector: 'BO/BObasePage.js',
-    filepath: '@versions/v178/BO/BObasePage.js',
+    file: 'BO/login/index.js',
+    combinations: {
+      common: {
+        type: 'filepath',
+        target: '@versions/common/BO/login/index.js',
+      },
+      '1.7.2.5': {
+        type: 'filepath',
+        filepath: '@versions/v172/BO/login/index.js',
+      },
+      '1.7.1.2': {
+        type: 'version',
+        filepath: '1.7.2.5',
+      },
+      '1.7.0.6': {
+        type: 'version',
+        filepath: '1.7.2.5',
+      },
+    },
   },
   {
-    version: '1.7.3.4',
-    selector: 'BO/BObasePage.js',
-    filepath: '@versions/v173/BO/BObasePage.js',
+    file: 'BO/dashboard/index.js',
+    combinations: {
+      common: {
+        type: 'filepath',
+        target: '@versions/common/BO/dashboard/index.js',
+      },
+    },
   },
   {
-    version: '1.7.2.5',
-    selector: 'BO/BObasePage.js',
-    filepath: '@versions/v172/BO/BObasePage.js',
+    file: 'BO/modules/moduleCatalog/index.js',
+    combinations: {
+      common: {
+        type: 'filepath',
+        target: '@versions/common/BO/modules/moduleCatalog/index.js',
+      },
+    },
   },
   {
-    version: '1.7.1.2',
-    selector: 'BO/BObasePage.js',
-    filepath: '@versions/v172/BO/BObasePage.js',
+    file: 'BO/modules/moduleManager/index.js',
+    combinations: {
+      common: {
+        type: 'filepath',
+        target: '@versions/common/BO/modules/moduleManager/index.js',
+      },
+      '1.7.4.4': {
+        type: 'filepath',
+        target: '@versions/v174/BO/modules/moduleManager/index.js',
+      },
+      '1.7.3.4': {
+        type: 'filepath',
+        target: '@versions/v173/BO/modules/moduleManager/index.js',
+      },
+      '1.7.2.5': {
+        type: 'version',
+        target: '1.7.3.4',
+      },
+      '1.7.1.2': {
+        type: 'filepath',
+        target: '@versions/v171/BO/modules/moduleManager/index.js',
+      },
+      '1.7.0.6': {
+        type: 'version',
+        target: '1.7.1.2',
+      },
+    },
   },
   {
-    version: '1.7.0.6',
-    selector: 'BO/BObasePage.js',
-    filepath: '@versions/v172/BO/BObasePage.js',
-  },
-  {
-    version: 'common',
-    selector: 'BO/login/index.js',
-    filepath: '@versions/common/BO/login/index.js',
-  },
-  {
-    version: '1.7.2.5',
-    selector: 'BO/login/index.js',
-    filepath: '@versions/v172/BO/login/index.js',
-  },
-  {
-    version: '1.7.1.2',
-    selector: 'BO/login/index.js',
-    filepath: '@versions/v172/BO/login/index.js',
-  },
-  {
-    version: '1.7.0.6',
-    selector: 'BO/login/index.js',
-    filepath: '@versions/v172/BO/login/index.js',
-  },
-  {
-    version: 'common',
-    selector: 'BO/dashboard/index.js',
-    filepath: '@versions/common/BO/dashboard/index.js',
-  },
-  {
-    version: 'common',
-    selector: 'BO/modules/moduleCatalog/index.js',
-    filepath: '@versions/common/BO/modules/moduleCatalog/index.js',
-  },
-  {
-    version: 'common',
-    selector: 'BO/modules/moduleManager/index.js',
-    filepath: '@versions/common/BO/modules/moduleManager/index.js',
-  },
-  {
-    version: '1.7.4.4',
-    selector: 'BO/modules/moduleManager/index.js',
-    filepath: '@versions/v174/BO/modules/moduleManager/index.js',
-  },
-  {
-    version: '1.7.3.4',
-    selector: 'BO/modules/moduleManager/index.js',
-    filepath: '@versions/v173/BO/modules/moduleManager/index.js',
-  },
-  {
-    version: '1.7.2.5',
-    selector: 'BO/modules/moduleManager/index.js',
-    filepath: '@versions/v173/BO/modules/moduleManager/index.js',
-  },
-  {
-    version: '1.7.1.2',
-    selector: 'BO/modules/moduleManager/index.js',
-    filepath: '@versions/v171/BO/modules/moduleManager/index.js',
-  },
-  {
-    version: '1.7.0.6',
-    selector: 'BO/modules/moduleManager/index.js',
-    filepath: '@versions/v171/BO/modules/moduleManager/index.js',
-  },
-  {
-    version: 'common',
-    selector: 'BO/modules/moduleConfiguration/index.js',
-    filepath: '@versions/common/BO/modules/moduleConfiguration/index.js',
+    file: 'BO/modules/moduleConfiguration/index.js',
+    combinations: {
+      common: {
+        type: 'filepath',
+        target: '@versions/common/BO/modules/moduleConfiguration/index.js',
+      },
+    },
   },
 ];
