adc2b299e8321c40f3d5446c67bdcebbac536d79
Simon Garny <simon.garny@prestashop.com>
2020-09-28 10:40:17 +0200

    add all folders and files for every version

---
+++ tests/units/data/configClassMap.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    file: 'BO/BObasePage.js',
+    versions: {
+      '1.7.5': '@versions/175/BO/BObasePage.js',
+    },
+  },
+  {
+    file: 'BO/login/index.js',
+    versions: {
+      '1.7.3': '@versions/173/BO/login/index.js',
+    },
+  },
+];
---
+++ versions/174/BO/modules/moduleManager/index.js
@@ -0,0 +1,74 @@
+const BOBasePage = require('../../BObasePage.js');
+
+class ModuleManager extends BOBasePage {
+  constructor() {
+    super();
+
+    this.pageTitle = 'Module manager •';
+
+    // Selectors
+    this.searchModuleTagInput = '#search-input-group input.pstaggerAddTagInput';
+    this.searchModuleButton = '#module-search-button';
+    this.modulesListBlock = '.module-short-list:not([style=\'display: none;\'])';
+    this.modulesListBlockTitle = `${this.modulesListBlock} span.module-search-result-title`;
+    this.allModulesBlock = `${this.modulesListBlock} .module-item-list`;
+    this.moduleBlock = moduleName => `${this.allModulesBlock}[data-name='${moduleName}']`;
+    this.disableModuleButton = moduleName => `${this.moduleBlock(moduleName)} button.module_action_menu_disable`;
+    this.configureModuleButton = moduleName => `${this.moduleBlock(moduleName)}`
+      + ' div.module-actions a[href*=\'/action/configure\']';
+    this.actionsDropdownButton = moduleName => `${this.moduleBlock(moduleName)} button.dropdown-toggle`;
+    // Status dropdown selectors
+    this.statusDropdownDiv = '#module-status-dropdown';
+    this.statusDropdownMenu = 'div.ps-dropdown-menu[aria-labelledby=\'module-status-dropdown\']';
+    this.statusDropdownItemLink = ref => `${this.statusDropdownMenu} ul li[data-status-ref='${ref}'] a`;
+    // Categories
+    this.categoriesSelectDiv = '#categories';
+    this.categoriesDropdownDiv = 'div.ps-dropdown-menu.dropdown-menu.module-category-selector';
+    this.categoryDropdownItem = cat => `${this.categoriesDropdownDiv} li[data-category-display-name='${cat}']`;
+  }
+
+  /*
+  Methods
+   */
+
+  /**
+   * Search Module in Page module Catalog
+   * @param page
+   * @param moduleTag, Tag of Module
+   * @param moduleName, Name of module
+   * @return {Promise<void>}
+   */
+  async searchModule(page, moduleTag, moduleName) {
+    await page.type(this.searchModuleTagInput, moduleTag);
+    await page.click(this.searchModuleButton);
+    return this.elementVisible(page, this.moduleBlock(moduleName), 10000);
+  }
+
+  /**
+   * Click on button configure of a module
+   * @param page
+   * @param moduleName, Name of module
+   * @return {Promise<void>}
+   */
+  async goToConfigurationPage(page, moduleName) {
+    if (await this.elementNotVisible(page, this.configureModuleButton(moduleName), 1000)) {
+      await Promise.all([
+        page.click(this.actionsDropdownButton(moduleName)),
+        this.waitForVisibleSelector(page, `${this.actionsDropdownButton(moduleName)}[aria-expanded='true']`),
+      ]);
+    }
+    await page.click(this.configureModuleButton(moduleName));
+  }
+
+  /**
+   * Get status of module (enable/disable)
+   * @param page
+   * @param moduleName
+   * @return {Promise<boolean>}
+   */
+  isModuleEnabled(page, moduleName) {
+    return this.elementNotVisible(page, this.disableModuleButton(moduleName), 1000);
+  }
+}
+
+module.exports = new ModuleManager();
