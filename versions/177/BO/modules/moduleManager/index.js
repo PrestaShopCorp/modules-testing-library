c9eb7f82104c7ea3859a1cd6b9c72cacdbe5b3ab
Boubker BRIBRI <boubker.bribri@prestashop.com>
2020-10-13 14:03:32 +0200

    Add update module function for 1.7.7

--- versions/177/BO/modules/moduleManager/index.js
+++ versions/177/BO/modules/moduleManager/index.js
@@ -10,30 +10,30 @@ class ModuleManager extends BOBasePage {
     this.searchModuleTagInput = '#search-input-group input.pstaggerAddTagInput';
     this.searchModuleButton = '#module-search-button';
     this.modulesListBlock = '.module-short-list:not([style=\'display: none;\'])';
-    this.modulesListBlockTitle = `${this.modulesListBlock} span.module-search-result-title`;
     this.allModulesBlock = `${this.modulesListBlock} .module-item-list`;
     this.moduleBlock = moduleName => `${this.allModulesBlock}[data-name='${moduleName}']`;
     this.disableModuleButton = moduleName => `${this.moduleBlock(moduleName)} button.module_action_menu_disable`;
     this.configureModuleButton = moduleName => `${this.moduleBlock(moduleName)}`
       + ' div.module-actions a[href*=\'/action/configure\']';
     this.actionsDropdownButton = moduleName => `${this.moduleBlock(moduleName)} button.dropdown-toggle`;
+
     // Status dropdown selectors
-    this.statusDropdownDiv = '#module-status-dropdown';
     this.statusDropdownMenu = 'div.ps-dropdown-menu[aria-labelledby=\'module-status-dropdown\']';
     this.statusDropdownItemLink = ref => `${this.statusDropdownMenu} ul li[data-status-ref='${ref}'] a`;
-    // Categories
+
+    // Categories selectors
     this.categoriesSelectDiv = '#categories';
     this.categoriesDropdownDiv = 'div.ps-dropdown-menu.dropdown-menu.module-category-selector';
     this.categoryDropdownItem = cat => `${this.categoriesDropdownDiv} li[data-category-display-name='${cat}']`;
-    // Upload
+
+    // Upload selectors
     this.uploadModuleButton = '#page-header-desc-configuration-add_module';
     this.uploadModuleModal = '#module-modal-import';
     this.uploadModuleModalCloseButton = '#module-modal-import-closing-cross';
-    this.uploadModuleModalFileInput = '#importDropzone > input';
-    this.uploadModuleModalProcessing = '#importDropzone > div.module-import-processing';
-    this.uploadModuleModalFailure = '#importDropzone > div.module-import-failure';
-    this.uploadModuleModalSuccess = '#importDropzone > div.module-import-success';
-    this.uploadModuleModalConfigureButton = '#importDropzone > div.module-import-success > a';
+    this.importDropZone = '#importDropzone';
+    this.uploadModuleModalFileInput = `${this.importDropZone} input`;
+    this.uploadModuleModalProcessing = `${this.importDropZone} > div.module-import-processing`;
+    this.uploadModuleModalSuccess = `${this.importDropZone} > div.module-import-success`;
   }
 
   /*
@@ -88,10 +88,13 @@ class ModuleManager extends BOBasePage {
   async uploadModule(page, filePath) {
     await page.click(this.uploadModuleButton);
     await this.waitForVisibleSelector(page, this.uploadModuleModal);
+
     const handle = await page.$(this.uploadModuleModalFileInput);
     await handle.setInputFiles(filePath);
+
     await page.waitForSelector(this.uploadModuleModalProcessing, {state: 'hidden'});
-    return this.elementVisible(page, this.uploadModuleModalSuccess);
+
+    return this.elementVisible(page, this.uploadModuleModalSuccess, 2000);
   }
 }
 
